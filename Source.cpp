607c95cb1ce05d91c2dcdc1c68282e6e55a96b85
Joao Faria <ei11103@fe.up.pt>
2016-11-01 19:13:03 +0000

    first commit

---
+++ Source.cpp
@@ -0,0 +1,127 @@
+/**
+ * @function findContours_Demo.cpp
+ * @brief Demo code to find contours in an image
+ * @author OpenCV team
+ */
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace cv;
+using namespace std;
+
+Mat src; Mat src_gray;
+int thresh = 40;
+int max_thresh = 255;
+RNG rng(12345);
+int houghlines(Mat src);
+
+/// Function header
+void thresh_callback(int, void* );
+
+/**
+ * @function main
+ */
+int maine( int, char** argv )
+{
+  /// Load source image
+  src = imread("test.jpg");
+  if (src.empty())
+  {
+    cerr << "No image supplied ..." << endl;
+    return -1;
+  }
+
+  /// Convert image to gray and blur it
+  cvtColor( src, src_gray, COLOR_BGR2GRAY );
+  blur( src_gray, src_gray, Size(3,3) );
+
+  /// Create Window
+  const char* source_window = "Source";
+  namedWindow( source_window, WINDOW_AUTOSIZE );
+  imshow( source_window, src );
+
+  createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
+  thresh_callback( 0, 0 );
+
+  waitKey(0);
+  return(0);
+}
+
+/**
+ * @function thresh_callback
+ */
+void thresh_callback(int, void* )
+{
+  Mat canny_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  /// Detect edges using canny
+  Canny( src_gray, canny_output, thresh, thresh*2, 3 );
+  /// Find contours
+  findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  /// Draw contours
+  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+  for( size_t i = 0; i< contours.size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
+     }
+
+  /// Show in a window
+  //namedWindow( "Contours", WINDOW_AUTOSIZE );
+  //imshow( "Contours", drawing );
+  houghlines(drawing);
+}
+
+int houghlines(Mat src)
+{
+
+    if(src.empty())
+    {
+        return -1;
+    }
+
+    Mat dst, cdst;
+    //Canny(src, dst, 50, 200, 3
+	Canny( src, dst, thresh, thresh*2, 3 );
+	blur(dst,dst,Size(2,2));
+    cvtColor(dst, cdst, COLOR_GRAY2BGR);
+
+#if 0
+    vector<Vec2f> lines;
+    HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );
+
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+    }
+#else
+    vector<Vec4i> lines;
+    HoughLinesP(dst, lines, 1, CV_PI/180, 40);
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        Vec4i l = lines[i];
+        line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+    }
+#endif
+    imshow("source", dst);
+    imshow("detected lines", cdst);
+
+    waitKey();
+
+    return 0;
+}
